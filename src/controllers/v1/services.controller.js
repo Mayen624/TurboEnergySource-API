686e173da071cdd6e714a2b1e6355715b2983b57
Mayen624 <juliomayen300@gmail.com>
2025-01-19 18:04:57 -0600

    services controller init and services model improved

--- src/controllers/v1/services.controller.js
+++ src/controllers/v1/services.controller.js
@@ -1,8 +1,30 @@
 import servicesShemma from "#models/v1/services.js";
-
+import validator from "#utils/v1/validator.js"
+import valData from "#utils/v1/ValidateData.js";
+import {paginate} from "#utils/v1/functions.js";
 
 const getServices = async (req,res) => {
-    
+
+    try {
+        const { page = 1, limit = 10 } = req.query;
+
+        const paginateData = await paginate(servicesShemma, page, limit);
+
+        if (paginateData.error) {
+            return res.status(500).json({ error: paginateData.error });
+        }
+
+        return res.status(200).json({
+            limit,
+            services: paginateData.data,
+            total: paginateData.total,
+            totalPages: paginateData.totalPages,
+            currentPage: paginateData.currentPage
+        });
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({ error: e.message });
+    }
 }
 
 const addService = async (req,res) => {
--- src/models/v1/services.js
+++ src/models/v1/services.js
@@ -2,14 +2,29 @@ import mongoose from "mongoose";
 
 const {model, Schema} = mongoose;
 
+const imageSchema = new Schema({
+    src: { type: String, required: true },
+    mime: { type: String, required: true },
+    name: { type: String, required: true },
+    orgName: { type: String, required: true },
+});
+
 const serviceShemma = new Schema({
     title       : {type: String, unique: true, required: true},
     description : {type: String, required: true},
-    createdBy   : {type: Date, required: true},
-    updatedBy   : {type: Date, required: true},
-    images      : {type: String},
-    enabled     : {type: Boolean, default: true}
-}, {timestamps: true});
+    images: {
+        type: [imageSchema],
+        validate: {
+          validator: function (value) {
+            return value.length >= 1 && value.length <= 2; // Validar que haya entre 1 y 2 imágenes
+          },
+          message: 'El array de imágenes debe contener entre 1 y 2 elementos.',
+        },
+    },
+    enabled     : {type: Boolean, default: true},
+    createdBy   : { type: mongoose.Types.ObjectId, required: true, ref: 'User'},
+    updatedBy   : { type: mongoose.Types.ObjectId, default: null, ref: 'User'},
+}, {timestamps  : true});
 
 const service = model('Services', serviceShemma);
 
